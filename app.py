e94e19164c77f9eecffa88b521eafbb68bf8c4be
Aditya Gupta <adityagupta.ad62001@gmail.com>
2022-11-04 08:48:24 +0530

    Flask API

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,jsonify,request
+from flask_pymongo import PyMongo
+from bson.json_util import loads, dumps
+from bson.objectid import ObjectId
+from send import publish
+import json
+app = Flask(__name__)
+app.secret_key = "secretkey"
+#app.config['MONGO_dbname'] = 'orders'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/pizza_house'
+mongo = PyMongo(app)
+
+
+@app.route('/welcome', methods=['GET'])
+def welcome():
+    return "Welcome to Pizza House"
+
+
+
+@app.route('/order', methods = ['POST'])
+def add_order():
+    if request.method == 'POST':
+        _data = request.json
+        _order = _data['order']
+        id = mongo.db.orders.insert_one({'order': _order})
+        print(id.inserted_id)
+        response = jsonify(f"Order Added success, OrderID :- f{id.inserted_id}")
+        response.status_code = 200
+        return response
+    else:
+        return jsonify(message="Not Allowed")
+
+@app.route('/getorders', methods = ['GET'])
+def get_orders():
+    _all_orders = mongo.db.orders.find()
+    _all_orders = dumps(_all_orders)
+    return _all_orders
+
+@app.route('/getorders/<order_id>', methods = ['GET'])
+def get_order(order_id):
+    _found_order = mongo.db.orders.find_one({"_id" : ObjectId(order_id)})
+    if _found_order is None:
+        return jsonify(message="Order Not Found")
+    return dumps(_found_order)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105, debug = True, use_reloader = True)
---
+++ send.py
@@ -0,0 +1,12 @@
+import pika
+import json
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+def publish(body):
+    #publishing order to RabitMq
+    channel.queue_declare(queue='order')
+    channel.basic_publish(exchange='', routing_key='order', body= json.dumps(body))
+    print(f"Sent the Request---{body}'")
+    connection.close()
+publish("This is me")
